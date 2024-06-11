cee8c444a4218d3f5f4a91c79c2be5c13aadb4e0
Bayu Putra <bayu.bahruddin@gmail.com>
2024-06-11 17:23:50 +0700

    feat: add data json

---
+++ data/femaleProfile.js
@@ -0,0 +1,47 @@
+[
+    {
+        "nama": "Siti Aminah",
+        "tanggal_lahir": "1990-05-24",
+        "tinggi_badan": 160,
+        "berat_badan": 55,
+        "image_url": "https://example.com/images/siti.jpg",
+        "pekerjaan": "Dokter",
+        "UserId": 2
+    },
+    {
+        "nama": "Dewi Lestari",
+        "tanggal_lahir": "1995-11-02",
+        "tinggi_badan": 165,
+        "berat_badan": 60,
+        "image_url": "https://example.com/images/dewi.jpg",
+        "pekerjaan": "Penulis",
+        "UserId": 4
+    },
+    {
+        "nama": "Linda Putri",
+        "tanggal_lahir": "1993-12-30",
+        "tinggi_badan": 158,
+        "berat_badan": 52,
+        "image_url": "https://example.com/images/linda.jpg",
+        "pekerjaan": "Desainer",
+        "UserId": 6
+    },
+    {
+        "nama": "Maya Sari",
+        "tanggal_lahir": "1991-04-20",
+        "tinggi_badan": 162,
+        "berat_badan": 57,
+        "image_url": "https://example.com/images/maya.jpg",
+        "pekerjaan": "Aktris",
+        "UserId": 8
+    },
+    {
+        "nama": "Nina Safitri",
+        "tanggal_lahir": "1996-01-14",
+        "tinggi_badan": 164,
+        "berat_badan": 59,
+        "image_url": "https://example.com/images/nina.jpg",
+        "pekerjaan": "Jurnalis",
+        "UserId": 10
+    }
+]
---
+++ data/maleProfile.js
@@ -0,0 +1,47 @@
+[
+    {
+        "nama": "Budi Santoso",
+        "tanggal_lahir": "1985-07-12",
+        "tinggi_badan": 175,
+        "berat_badan": 70,
+        "image_url": "https://example.com/images/budi.jpg",
+        "pekerjaan": "Insinyur",
+        "UserId": 1
+    },
+    {
+        "nama": "Andi Wijaya",
+        "tanggal_lahir": "1982-03-10",
+        "tinggi_badan": 180,
+        "berat_badan": 80,
+        "image_url": "https://example.com/images/andi.jpg",
+        "pekerjaan": "Pengacara",
+        "UserId": 3
+    },
+    {
+        "nama": "Rudi Hartono",
+        "tanggal_lahir": "1988-08-19",
+        "tinggi_badan": 170,
+        "berat_badan": 75,
+        "image_url": "https://example.com/images/rudi.jpg",
+        "pekerjaan": "Pengusaha",
+        "UserId": 5
+    },
+    {
+        "nama": "Arif Setiawan",
+        "tanggal_lahir": "1987-06-15",
+        "tinggi_badan": 172,
+        "berat_badan": 68,
+        "image_url": "https://example.com/images/arif.jpg",
+        "pekerjaan": "Guru",
+        "UserId": 7
+    },
+    {
+        "nama": "Rizal Fahmi",
+        "tanggal_lahir": "1984-09-05",
+        "tinggi_badan": 178,
+        "berat_badan": 72,
+        "image_url": "https://example.com/images/rizal.jpg",
+        "pekerjaan": "Arsitek",
+        "UserId": 9
+    }
+]
